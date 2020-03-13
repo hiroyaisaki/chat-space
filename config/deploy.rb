28f473f219ea8f22c5542c0c936d76e52862be6a
hiroyaisaki <60531920+hiroyaisaki@users.noreply.github.com>
2020-03-13 22:28:36 +0900

    deploy-restore

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,6 +26,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
@@ -43,9 +44,4 @@ namespace :deploy do
   end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
-end
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
 end
